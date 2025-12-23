a3fab92eb83ac308bace2f00beb9e89f304cbd8b
Titoluwa <titoluwaranti@gmail.com>
2025-12-23 11:44:55 +0100

    fix: Scroll Homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({
       </head> */}
       <body>
         <ClientProviders>
-          {/* <ScrollToTop /> */}
+          <ScrollToTop />
           {/* <LoadingScreen /> */}
           <Toaster />
           <Sonner />
