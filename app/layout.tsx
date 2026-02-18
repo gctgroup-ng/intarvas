e0a6c1da437a991d288ead5d03a1633eda04158f
Titoluwa <titoluwaranti@gmail.com>
2026-02-18 09:08:35 +0100

    fix: google tag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import "./globals.css";
 import { ClientProviders } from "@/components/providers/ClientProviders";
 import SiteHeader from "@/components/layout/SiteHeader";
 import SiteFooter from "@/components/layout/SiteFooter";
-import LoadingScreen from "@/components/common/LoadingScreen";
+// import LoadingScreen from "@/components/common/LoadingScreen";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import SupportWidget from "@/components/common/SupportWidget";
+// import SupportWidget from "@/components/common/SupportWidget";
 import ScrollToTop from "@/components/ScrollToTop";
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -95,25 +95,22 @@ export default function RootLayout({
         />
       </head> */}
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PVTZNJHQVV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5S9FTKDR');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PVTZNJHQVV');
           `}
         </Script>
       </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5S9FTKDR"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <ClientProviders>
           <ScrollToTop />
           {/* <LoadingScreen /> */}
