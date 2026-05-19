aeb207401e366ef2277208b7c8bd6aa17a609b32
Titoluwa <titoluwaranti@gmail.com>
2026-05-19 12:34:25 +0100

    fix: update GOOGLE TAGS again

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,20 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9XZQ2FFR5J"></Script>
+        <Script
+          id="ga-config"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-9XZQ2FFR5J');
+            `,
+          }}
+        />
         {/* GTM Head Snippet */}
         <Script
           id="gtm-head"
